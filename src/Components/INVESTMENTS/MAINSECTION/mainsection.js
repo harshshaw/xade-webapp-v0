0cbdbadfd8f16d59f878d086c26cddfc3d680c8d
Arnav-Jhajharia <jhajhariaarnav@gmail.com>
2022-07-10 17:40:08 +0530

    pr

--- src/Components/INVESTMENTS/MAINSECTION/mainsection.js
+++ src/Components/INVESTMENTS/MAINSECTION/mainsection.js
@@ -1,10 +1,35 @@
+import { findAllByTestId } from '@testing-library/react';
 import React from 'react';
+import styles from './mainsection.module.css'
+import Overview from './SubComp/Overview.jsx'
+import Trade from './SubComp/Trade.jsx'
+
 
 export default function Component() {
-    return (
+    let [state, setState] = React.useState(true);
 
+
+
+
+
+    return (
+        
         <>
+            <div className = {styles.container}>
+                     <div className = {styles.toggleBar}>
+                        <button onClick = {() => setState(true)} className = {styles.overviewClick + "  " + (state?styles.highlight:'')}>Overview</button>
+                        <button onClick = {() => setState(false)} className = {styles.tradeClick + "  " + ((!state)?styles.highlight:'')}>Trade</button>
+                     </div>
+            </div>
+
+            <div className = {styles.yeahthis}>
+                    {state ? 
+                        <Overview />
+                            :
+                        <Trade />
+                    }
+                </div>
         </>
     
         )
-}
+}   
